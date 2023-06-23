50484a7917b87708620e494a37db7feac104e4f3
unknown <shaikhshahbaz4022@gmail.com>
2023-06-23 21:53:11 +0530

    SignUp-Login-Logout Added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,1 +1,11 @@
-//user model
+const mongoose = require('mongoose');
+const userObj = {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+}
+const userSchema = mongoose.Schema(userObj)
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = { userModel }
